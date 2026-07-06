9e88cd68c620ae2b3b9809f031f53f22d5fc64a6
kaihongoh <kaihongoh5@gmail.com>
2026-07-06 17:39:02 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,26 @@
+<?php 
+// take active category from database
+$category_sql = "SELECT Category_ID, Category_Name FROM category WHERE Status = 'Active' ORDER BY Category_Name";
+$category_result = mysqli_query($conn, $category_sql);
+$categories = [];
+if ($category_result) {
+    while($cat = mysqli_fetch_assoc($category_result)) {
+        $categories[] = $cat;
+    }
+}
+?>
+
     <footer class="footer">
       <div class="footer-container">
         <div class="footer-about">
           <img
-            src="image\Ikea4U-logo (1).png"
-            alt="Lakeshow Grocer"
+            src="image\footer_new_logo.jpeg"
+            alt="HomeNest"
             class="footer-logo"
           />
           <p style="text-align:justify;">
             Some clubs are for the select few, 
-            but IKEA Family is for everyone. 
+            but HomeNest Family is for everyone. 
             Become a Family member and enjoy rewards, discounts, 
             inspirations and a few surprises all year round.
           </p>
@@ -30,43 +42,25 @@
         <div class="footer-links">
           <h3 class="footer-heading">Quick Links</h3>
           <ul class="footer-links">
-            <li><a href="main page.php">Home</a></li>
-            <li><a href="aboutUs.php">About Us</a></li>
-            <li><a href="C_Order_History.php">Order History</a></li>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="AboutUs.php">About Us</a></li>
+            <li><a href="order_history.php">Order History</a></li>
           </ul>
         </div>
 
         <div class="footer-links">
           <h3 class="footer-heading">Categories</h3>
           <ul class="footer-links">
-            <li>
-              <a href="get_products_by_category.php?category=1"
-                >Armchairs</a
-              >
-            </li>
-            <li>
-              <a href="get_products_by_category.php?category=2"
-                >Sofas</a
-              >
-            </li>
-            <li>
-              <a href="get_products_by_category.php?category=3">Storage Boxes & Baskets</a>
-            </li>
-            <li>
-              <a href="get_products_by_category.php?category=4">Beds & Mattresses</a>
-            </li>
-            <li>
-              <a href="get_products_by_category.php?category=5">Kitchen Kabinets</a>
-            </li>
-            <li>
-              <a href="get_products_by_category.php?category=6"
-                >Laundry & Cleaning</a
-              >
-            </li>
-            <li>
-              <a href="get_products_by_category.php?category=7">Home Decoration</a>
-            </li>
-
+          <?php if (!empty($categories)): ?>
+                <?php foreach ($categories as $category): ?>
+                      <li>
+                        <a 
+                            href="product.php?category=<?= htmlspecialchars($category['Category_ID']) ?>">
+                            <?= htmlspecialchars($category['Category_Name']); ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+                <?php endif; ?>
           </ul>
         </div>
 
@@ -77,16 +71,16 @@
             47800 Petaling Jaya, Selangor
           </p>
           <p> 03-7952 7575</p>
-          <p>customerservice.ikeamy@ikano.asia</p>
+          <p>customerservice.homenest@homenest.asia</p>
           <p>Mon-Sun: 9:30 am - 10:00 pm</p>
           <p style="text-align:justfy;">
-            For IKEA Business inquiries, you can reach them at +603 7730 0194 or via email at business.ikeamy@ikano.asia. 
+            For HomeNest Business inquiries, you can reach them at +603 7730 0194 or via email at business.homenest@homenest.asia. 
           </p>
 
           <h4 style="color: white; margin-top: 20px">We Accept</h4>
           <div class="payment-methods">
             <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/2560px-Visa_Inc._logo.svg.png"
+              src="image\visa.png"
               alt="Visa"
               style="height: 30px; margin-right: 10px"
             />
@@ -101,7 +95,7 @@
 
       <div class="footer-copyright">
         <p>
-          &copy; 2026 Ikea4U. All Rights Reserved. |
+          &copy; 2026 HomeNest. All Rights Reserved. |
           <a
             href="#"
             >Privacy Policy</a
