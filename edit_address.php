bbe591b392820498b37caf4e3800c4a893eb8a7f
kaihongoh <kaihongoh5@gmail.com>
2026-07-06 17:38:18 +0800

    Update edit_address.php

--- edit_address.php
+++ edit_address.php
@@ -2,7 +2,7 @@
 require_once 'includes/config.php';
 session_start();
 
-// Check if ID is provided (when type from browser), if error will redirect back to product list
+// Check if ID is provided (when type from browser), if error will redirect back
 if (!isset($_GET['id']) || empty($_GET['id'])) {
     header("Location: login.php");
     exit();
@@ -12,10 +12,11 @@ $user_id=$_SESSION["user_id"];
 $address_id=isset($_GET["id"]) ? (int)$_GET["id"] : 0;
 
 // fetch address data
-$stmt = $conn->prepare("SELECT * FROM user_address WHERE Address_ID = ? AND User_ID=?");
-$stmt->bind_param("ii", $address_id, $user_id);
-$stmt->execute();
-$result = $stmt->get_result()->fetch_assoc();
+$get_address = $conn->prepare("SELECT * FROM user_address WHERE Address_ID = ? AND User_ID=?");
+$get_address->bind_param("ii", $address_id, $user_id);
+$get_address->execute();
+$address = $get_address->get_result()->fetch_assoc();
+$address_data=$address;
 
 if(!$address) {
     $_SESSION["error"] = "Address not found.";
@@ -26,10 +27,12 @@ if(!$address) {
 $full_name= $address['Full_Name'];
 $phone= $address['Phone'];
 $state = $address['State'];
+$city = $address['city'];
 $postcode = $address['postcode'];
-$address= $address['Address'];
+$new_address_line1= $address['address_line1'];
+$new_address_line2= $address['address_line2'];
 $unit_no= $address['Unit_No'];
-$delivery_notes= $address['Delivery_Notes'];
+$remarks= $address['Remarks'];
 $label= $address['Label'];
 $is_default= $address['Is_Default'];
 
@@ -41,10 +44,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $full_name= trim($_POST['full_name'] ?? '');
     $phone= trim($_POST['phoneNumber'] ?? '');
     $state = trim($_POST['state'] ?? '');
+    $city = trim($_POST['city'] ?? '');
     $postcode = trim($_POST['postcode'] ?? '');
-    $address= trim($_POST['address'] ?? '');
+    $new_address_line1= trim($_POST['address_line1'] ?? '');
+    $new_address_line2= trim($_POST['address_line2'] ?? '');
     $unit_no= trim($_POST['unit_no'] ?? '');
-    $delivery_notes= trim($_POST['delivery_notes'] ?? '');
+    $remarks= trim($_POST['remarks'] ?? '');
     $label=$_POST['label'] ?? 'Home';
     $is_default= isset($_POST['is_default']) ?1:0;
 
@@ -62,30 +67,39 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $errors[] = 'Please select the state.';
     }
 
+    if(empty($city)){
+        $errors[] = 'Please select the city.';
+    } 
+
     if (empty($postcode)) {
         $errors[] = 'Please enter the postcode.';
     } elseif (!preg_match('/^\d{5}$/', $postcode)) {
         $errors[] = 'Postcode must be 5 digits.';
     }
 
-    if (empty($address)) {
-        $errors[] = 'Please enter the complete address.';
-    } elseif (strlen($address) < 10) {
-        $errors[] = 'Address must be at least 10 characters.';
+    if (empty($new_address_line1)) {
+        $errors[] = 'Please enter the address line 1 field.';
+    } elseif (strlen($new_address_line1) < 5) {
+        $errors[] = 'Address must be at least 5 characters.';
+    }
+
+    if($address_data['Is_Default']== 1 && $is_default==0){
+        $errors[] = 'You must have at least one default address.';
     }
 
    if(empty($errors)) {    
         if($is_default) { 
             $reset= $conn->prepare("UPDATE user_address SET Is_Default=0 WHERE User_ID=? AND Address_ID !=?");
-            $reset->bind_param('i', $user_id, $address_id);
+            $reset->bind_param('ii', $user_id, $address_id);
             $reset->execute();
         }
         $update= $conn->prepare('UPDATE user_address
-        SET Full_Name=?, Phone=?, State=?, postcode=?, Address=?, Unit_No=?, Delivery_Notes=?, Label=?, Is_Default=?, 
+        SET Full_Name=?, Phone=?, State=?, city=?, postcode=?, address_line1=?, address_line2=?, 
+        Unit_No=?, Remarks=?, Label=?, Is_Default=?
         WHERE Address_ID=? AND User_ID=?');
-        $update->bind_param('ssssssssi', $full_name, $phone, 
-        $state, $postcode, $address, $unit_no, 
-        $delivery_notes, $label, $is_default, $address_id, $user_id);
+        $update->bind_param('ssssssssssiii', $full_name, $phone, 
+        $state, $city, $postcode, $new_address_line1, $new_address_line2, $unit_no, 
+        $remarks, $label, $is_default, $address_id, $user_id);
 
         if($update->execute()) {
             $_SESSION['success']="Address update successfully!";
@@ -97,6 +111,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 
+$get_city=$conn->query("SELECT state_name, city_name FROM cities ORDER BY state_name, city_name");
+$city_by_state=[];
+while($city_result=$get_city->fetch_assoc()){
+    $state_data=$city_result['state_name'];
+    $city_data=$city_result['city_name'];
+    if(!isset($city_by_state[$state_data])){
+        $city_by_state[$state_data]=[];
+    }
+    $city_by_state[$state_data][]=$city_data;
+}
 ?>
 
 <!DOCTYPE html>
@@ -112,22 +136,26 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <body>
     <?php include_once 'includes/header.php'; ?>
 
-    <session class="register-page">
+    <section class="register-page">
         <div class="register-container">
             <div class="form-header">
-                <h2 class="register-title">Edit Product</h2>
+                <h2 class="register-title">Edit Address</h2>
                 <p class="form-subtitle">Update your shipping details</p>
             </div>
-            <?php if (!empty($errors)): ?>
-                <div class="error-message" style="color:red; border:1px solid red; padding:10px; margin-bottom:20px;">
-                    <?=implode('<br>', array_map('htmlspecialchars',$errors)) ?>
+            <?php if (!empty($errors)): ?>  
+                <div class="alert alert-error">
+                                <ul>
+                    <?php foreach ($errors as $error): ?>
+                        <li><?= htmlspecialchars($error); ?></li>
+                    <?php endforeach; ?>
+                </ul>
                 </div>
             <?php endif; ?>
 
             <form method="POST" action="" class="register-form" id="addressForm" novalidate>
             <div class="form-group">
                 <label class="form-label">Full Name </label>
-                <input type="text" name="full_name" class="form-control" 
+                <input type="text" id="full_name" name="full_name" class="form-control" 
                     value="<?= htmlspecialchars($full_name) ?>" 
                     placeholder="Enter your full name" required>
                 <small class="error-message" id="fullNameError"></small>
@@ -135,7 +163,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
             <div class="form-group">
                 <label class="form-label">Phone Number </label>
-                <input type="tel" name="phoneNumber" class="form-control"
+                <input type="tel" id="phoneNumber" name="phoneNumber" class="form-control"
                         value="<?= htmlspecialchars($phone) ?>" 
                         pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
                         placeholder="123-456-7890" required>
@@ -144,28 +172,48 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             </div>
 
             <div class="form-group">
-                <label class="form-label">State </label>
-                <select name="state" id ="stateSelect" class="form-control" required>
-                    <option value="">Select your state</option>
-                    <option value="Johor"<?=$state=='Johor'?'selected':''?>>Johor</option>
-                    <option value="Kedah"<?=$state=='Kedah'?'selected':''?>>Kedah</option>
-                    <option value="Kelantan"<?=$state=='Kelantan'?'selected':''?>>Kelantan</option>
-                    <option value="Malacca"<?=$state=='Malacca'?'selected':''?>>Malacca</option>
-                    <option value="Negeri Sembilan"<?=$state=='Negeri Sembilan'?'selected':''?>>Negeri Sembilan</option>
-                    <option value="Pahang"<?=$state=='Pahang'?'selected':''?>>Pahang</option>
-                    <option value="Perak"<?=$state=='Perak'?'selected':''?>>Perak</option>
-                    <option value="Perlis"<?=$state=='Perlis'?'selected':''?>>Perlis</option>
-                    <option value="Penang"<?=$state=='Penang'?'selected':''?>>Penang</option>
-                    <option value="Sabah"<?=$state=='Sabah'?'selected':''?>>Sabah</option>
-                    <option value="Sarawak"<?=$state=='Sarawak'?'selected':''?>>Sarawak</option>
-                    <option value="Selangor"<?=$state=='Selangor'?'selected':''?>>Selangor</option>
-                    <option value="Terengganu"<?=$state=='Terengganu'?'selected':''?>>Terengganu</option>
-                    <option value="Wilayah Persekutuan Kuala Lumpur"<?=$state=='Wilayah Persekutuan Kuala Lumpur'?'selected':''?>>Wilayah Persekutuan Kuala Lumpur</option>
-                    <option value="Wilayah Persekutuan Putrajaya"<?=$state=='Wilayah Persekutuan Putrajaya'?'selected':''?>>Wilayah Persekutuan Putrajaya</option>
-                    <option value="Wilayah Persekutuan Labuan"<?=$state=='Wilayah Persekutuan Labuan'?'selected':''?>>Wilayah Persekutuan Labuan</option>
-                </select>
-                <small class="error-message" id="stateError"></small>
+                <label class="form-label">Address Line 1</label>
+                <input type="text" name="address_line1" id="address" class="form-control"
+                    value="<?= htmlspecialchars($new_address_line1) ?>"
+                    placeholder="12, Jalan Bukit Beruang 5" required>
+                <small class="password-hint">Do not include the state and postcode</small>
+                <small class="error-message" id="addressError"></small>
+            </div>            
+            <div class="form-group">    
+                <label class="form-label">Address Line 2 (Optional)</label>
+                <input type="text" name="address_line2" class="form-control"
+                    value="<?= htmlspecialchars($new_address_line2) ?>"
+                    placeholder="Taman Bukit Melaka">
             </div>
+
+   <?php 
+    $get_states=$conn->prepare("SELECT state_name FROM shipping_fee_setting WHERE status='Active'
+    ORDER BY state_name ASC");
+    $get_states->execute();
+    $states=$get_states->get_result();
+    ?>
+
+    <div class="form-group">
+        <label class="form-label">State </label>
+        <select name="state" id ="stateSelect" class="form-control" required>
+            <option value="">Select your state</option>
+            <?php while($STATE=$states->fetch_assoc()): ?>
+                <option value="<?=htmlspecialchars($STATE['state_name'])?>"
+                <?=($state==$STATE['state_name']) ?'selected':''?>> <!--drop down-->
+                <?=htmlspecialchars($STATE['state_name'])?> 
+                </option>
+            <?php endwhile; ?>
+        </select>
+        <small class="error-message" id="stateError"></small>
+    </div>    
+    
+    <div class="form-group">
+        <label class="form-label">City </label>
+        <select name="city" id ="citySelect" class="form-control" required>
+            <option value="">Select your city</option>
+        </select> <!--load city will auto show the city for that state-->
+        <small class="error-message" id="cityError"></small>
+    </div>
 
             <div class="form-group">
                 <label class="form-label">Postcode</label>
@@ -176,27 +224,20 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 <small class="error-message" id="postcodeError"></small>
             </div>
                         
-            <div class="form-group">
-                <label class="form-label">Address</label>
-                <textarea name="address" class="form-control" rows="3" 
-                    placeholder="Enter your full address" required><?= htmlspecialchars($address) ?>
-                </textarea>
-                <small class="password-hint">Please provide complete address for delivery</small>
-                <small class="error-message" id="addressError"></small>
-            </div>
+
 
             <div class="form-group">
                 <label class="form-label">Unit No (optional)</label>
                 <input type="text" name="unit_no" id="unit_no" class="form-control"
                 value="<?=htmlspecialchars($unit_no) ?>"
-                placeholder="example: A-12-1, Block B"/>
+                placeholder="A-12-1, Block B"/>
             </div>
 
             <div class="form-group">
-                <label class="form-label">Comment (optional)</label>
-                <input type="text" name="delivery_notes" id="delivery_notes" class="form-control"
-                value="<?=htmlspecialchars($delivery_notes) ?>"
-                placeholder="example: put on the gate"/>
+                <label class="form-label">Remark (optional)</label>
+                <input type="text" name="remarks" id="remarks" class="form-control"
+                value="<?=htmlspecialchars($remarks) ?>"
+                placeholder="put on the gate"/>
             </div>
 
             <div class="form-group">
@@ -213,15 +254,20 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     <?=$is_default ? 'checked':''?>>Set as default address
                 </label>
             </div>
-            <button type="submit" class="btn-register">Add address</button>
-            <a href="address_list.php" class="btn-secondary" 
-            style="display:inline-block; margin-top:10px;">Cancel</a>
+            <button type="submit" id="submitBtn" class="btn-register">Save address</button>
+            <a href="address_list.php" class="cancel-btn">Cancel</a>
             </form>
         </div>
-    </session>
+    </section>
     
     <?php include_once 'includes/footer.php'; ?>
     
-    <script src="js/address.js"></script>    
+    <script>
+    const cityByState=<?php echo json_encode($city_by_state); ?>;
+    const selectedCity=<?php echo json_encode($city); ?>;
+    </script>
+
+    <script src="js/address.js"></script> 
+    <script src="js/alert.js"></script>   
 </body>
 </html>
