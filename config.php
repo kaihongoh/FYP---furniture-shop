bfc7ec730b309580c1432394cd69e0926212ab23
kaihongoh <kaihongoh5@gmail.com>
2026-07-06 17:38:40 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "fyp_furniture_shop";
 
+date_default_timezone_set('Asia/Kuala_Lumpur');
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
